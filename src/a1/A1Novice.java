6435f93d8ca357ec177d9f191f93cdb5c316b688
ellajuengst <ellajuengst@mix2s-mix2s.wireless-1x.unc.edu>
2019-08-27 10:55:03 -0400

    A1 Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,31 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		
+		int count = scan.nextInt();
+		
+		
+		for (int i=0; i<count; i++) {
+			String first = scan.next();
+			String last = scan.next();
+			double total = 0;
+			int quant = scan.nextInt();
+			for (int j=0; j<quant; j++) {
+				int itemquant = scan.nextInt();
+				scan.next();
+				double itemprice = scan.nextDouble();
+				double totprice = itemquant * itemprice;
+				total += totprice;
+			}
+			String firstinitial = first.substring(0,1);
+			System.out.println(firstinitial + ". " + last + " " + String.format("%.2f", total));
+		}
+		
+	
+		
+		
+		scan.close();
+		
 		
 	}
 }
