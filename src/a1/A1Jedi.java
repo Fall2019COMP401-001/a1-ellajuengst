490e722d6f7a011e5959a6c48ac79bfb117deca6
ellajuengst <ellajuengst@mix2s-mix2s.wireless-1x.unc.edu>
2019-08-27 12:57:25 -0400

    A1 Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,6 +9,42 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int items = scan.nextInt();
+		String[] foodnames = new String[items];
+		for (int i=0; i<items; i++) {
+			foodnames[i] = scan.next();
+			scan.nextDouble();
+		}
+		int customers = scan.nextInt();
+		int[] quantForEachItem = new int[items];
+		int[] numberOfCustPerItem = new int[items];
+		for (int i=0; i<customers; i++) {
+			scan.next();
+			scan.next();
+			int quantforCust = scan.nextInt();
+			for (int j=0; j<quantforCust; j++) {
+				int itemquant = scan.nextInt();
+				String itemname = scan.next();
+				for (int h=0; h<items; h++) {
+					if (itemname.equals(foodnames[h])) {
+						quantForEachItem[h] += itemquant;
+						numberOfCustPerItem[h] += 1;
+					}
+				}
+			}
+		}
+		scan.close();
+		
+		//have three arrays: one with food names in order, one with quantity of each item, one with number of customers for each item
+		
+		for (int i=0; i<items; i++) {
+			if (numberOfCustPerItem[i] == 0) {
+				System.out.println("No customers bought " + foodnames[i]);//then print "no ...."
+			} else {
+				System.out.println(numberOfCustPerItem[i] + " customers bought " + quantForEachItem[i] + " " + foodnames[i]);//then print number of person and name and number bought
+			}
+		}
+		
 		
 	}
 }
