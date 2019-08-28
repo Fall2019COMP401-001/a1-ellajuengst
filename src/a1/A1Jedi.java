fabc3d94d664e9cf149377d8aa0643373962983c
ellajuengst <ellajuengst@152.23.53.141>
2019-08-28 17:59:36 -0400

    A1 Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,42 +9,65 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		
 		int items = scan.nextInt();
 		String[] foodnames = new String[items];
+		
 		for (int i=0; i<items; i++) {
 			foodnames[i] = scan.next();
 			scan.nextDouble();
 		}
+		
 		int customers = scan.nextInt();
 		int[] quantForEachItem = new int[items];
 		int[] numberOfCustPerItem = new int[items];
+		
 		for (int i=0; i<customers; i++) {
-			scan.next();
-			scan.next();
+			String name = scan.next() + " " + scan.next();
 			int quantforCust = scan.nextInt();
+			int[] custsItemsNumbers = new int[items];
 			for (int j=0; j<quantforCust; j++) {
 				int itemquant = scan.nextInt();
 				String itemname = scan.next();
 				for (int h=0; h<items; h++) {
 					if (itemname.equals(foodnames[h])) {
 						quantForEachItem[h] += itemquant;
-						numberOfCustPerItem[h] += 1;
+						custsItemsNumbers [h] += 1;
 					}
+
+				}
+				
+			}
+			for (int h=0; h<items; h++) {
+				if (custsItemsNumbers [h]>= 1) {
+					numberOfCustPerItem[h] +=1;
 				}
 			}
-		}
-		scan.close();
+		}	
 		
-		//have three arrays: one with food names in order, one with quantity of each item, one with number of customers for each item
+	scan.close();
 		
-		for (int i=0; i<items; i++) {
-			if (numberOfCustPerItem[i] == 0) {
-				System.out.println("No customers bought " + foodnames[i]);//then print "no ...."
+	
+		
+		for (int p=0; p<items; p++) {
+			if (numberOfCustPerItem[p] == 0) {
+				System.out.println("No customers bought " + foodnames[p]);
 			} else {
-				System.out.println(numberOfCustPerItem[i] + " customers bought " + quantForEachItem[i] + " " + foodnames[i]);//then print number of person and name and number bought
+				System.out.println(numberOfCustPerItem[p] + " customers bought " + quantForEachItem[p] + " " + foodnames[p]);
 			}
 		}
 		
 		
 	}
-}
+	}
+
+
+	/* static int didTheyBuy(String name, String[] foods, int quant) {
+		for (int j=0; j<quant; j++) {
+			if (name.equals(foods[j])) {
+				return 1;
+			} 
+		}
+		return 0;
+	}*/
+
